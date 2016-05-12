3143eb9fda282fb7247c4d2f2c224d4a20b70b76
Sanjana Rao <sanjanar@thoughtworks.com>
2016-05-13 00:22:42 +0530

    First commit

---
+++ src/main/java/com/mycontactbook/controller/ContactsController.java
@@ -0,0 +1,110 @@
+package com.mycontactbook.controller;
+
+import com.mycontactbook.domain.Contact;
+import com.mycontactbook.repository.MyMongoTemplate;
+import com.mycontactbook.services.ContactOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.UnknownHostException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class ContactsController {
+    ContactOperations contactOperations = new ContactOperations(MyMongoTemplate.getMongoConnection());
+
+//    @RequestMapping(method = RequestMethod.GET)
+//    public String homePage(ModelMap modelMap) {
+//        List<Contact> allContacts = contactOperations.getAllContacts();
+//        modelMap.addAttribute("contacts", allContacts);
+//        return "index.htm";
+//    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getIndexPage() {
+        return "index";
+
+    }
+    @RequestMapping(method = RequestMethod.GET, value = "addNewContact")
+    public String addNewContact() {
+        return "addNewContact";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "add")
+    public String addContact(HttpServletRequest request, ModelMap modelMap) {
+        String name = request.getParameter("name");
+        int number = Integer.parseInt(request.getParameter("mobileNumber"));
+        String street1 = request.getParameter("street1");
+        String street2 = request.getParameter("stree2");
+        String city = request.getParameter("city");
+        String emailId = request.getParameter("emailId");
+        Contact contact = new Contact(name, number, street1, street2, city, emailId);
+        contactOperations.addContact(contact);
+        modelMap.addAttribute("message", "Contact Added Successfully");
+        List<Contact> allContacts = contactOperations.getAllContacts();
+        modelMap.addAttribute("contacts", allContacts);
+        return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "update")
+    public String updateContact(HttpServletRequest request, ModelMap modelMap) {
+
+        String name = request.getParameter("name");
+        int number = Integer.parseInt(request.getParameter("mobileNumber"));
+        String street1 = request.getParameter("street1");
+        String street2 = request.getParameter("street2");
+        String city = request.getParameter("city");
+        String emailId = request.getParameter("emailId");
+        Contact contact = new Contact(name, number, street1, street2, city, emailId);
+        try {
+            contactOperations.updateContact(contact);
+        } catch (UnknownHostException e) {
+            System.out.printf("Failed to update");
+        }
+        modelMap.addAttribute("message", "contact updated successfully");
+        List<Contact> allContacts = contactOperations.getAllContacts();
+        modelMap.addAttribute("contacts", allContacts);
+        return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getContact")
+    public String getContact(HttpServletRequest request, ModelMap modelMap) {
+
+        String emailId = request.getParameter("emailId");
+        Contact contact = contactOperations.getContact(emailId);
+        modelMap.addAttribute("contact", contact);
+        return "editContact";
+
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "delete")
+    public String deleteContact(HttpServletRequest request, ModelMap modelMap) {
+        String emailId=request.getParameter("emailId");
+        contactOperations.deleteContact(emailId);
+        modelMap.addAttribute("message", "Contact Deleted");
+        List<Contact> allContacts = contactOperations.getAllContacts();
+        modelMap.addAttribute("contacts", allContacts);
+        return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getUserDetail")
+    public String getUserDetailForm(HttpServletRequest request,ModelMap modelMap){
+        String emailId = request.getParameter("emailId");
+        Contact contact = contactOperations.getContact(emailId);
+        modelMap.addAttribute("contact", contact);
+        return "userDetail";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getContactByNameOrNumber")
+    public String getContactByNameOrNumber(HttpServletRequest request, ModelMap modelMap){
+        String searchContact = request.getParameter("searchContact");
+        List<Contact> contacts=contactOperations.searchContact(searchContact);
+        modelMap.addAttribute("contacts",contacts);
+        modelMap.addAttribute("message","search results");
+        return "searchResult";
+    }
+}
---
+++ target/First Ember/WEB-INF/pages/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sanjanar
+  Date: 12/05/16
+  Time: 10:49 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>jsp File</title>
+</head>
+<body>
+<h2>jsp file</h2>
+</body>
+</html>
